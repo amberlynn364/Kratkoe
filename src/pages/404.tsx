829a92addfb9f10ef81e1bc2906b25b9217b4e9f
Nikita Khrustalev <65180406+SadMearise@users.noreply.github.com>
2023-08-19 01:19:07 +0300

    feat: add html layout to 404 page (#36)
    * feat: add layout to 404 page
    
    * refactor: replace path

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,3 +1,19 @@
+import Button from "@mui/material/Button";
+import { Link } from "react-router-dom";
+import RouterPaths from "../router/routes";
+
 export default function Page404() {
-  return <h1>404</h1>;
+  return (
+    <div>
+      <h1>404</h1>
+      <p>We can&apos;t find the page you&apos;re looking for.</p>
+      <Button
+        variant="contained"
+        component={Link}
+        to={RouterPaths.Home}
+      >
+        Go home
+      </Button>
+    </div>
+  );
 }
