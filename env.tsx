48c00df2921448ff8aa9502f0ce72b620b16dc04
zena86 <76158579+zena86@users.noreply.github.com>
2023-09-04 23:54:46 +0300

    fix: change env, hasValidToken (#61)
    * fix: change hasValidToken
    
    * fix: change env
    
    ---------
    
    Co-authored-by: Yauheniya Mastavaya <jk@db.by>

--- env.tsx
+++ env.tsx
@@ -1,10 +1,11 @@
 export const CTP_PROJECT_KEY = "kratkoe";
-export const CTP_CLIENT_SECRET = "s78hr1YhzRFKblVf807L6mTyauNGSzJA";
-export const CTP_CLIENT_ID = "nlLV9TI7HS45nXhiJnmtXM1m";
+export const CTP_CLIENT_SECRET = "rN8nZp1MBlBlXS4MwaN2PKaA7wrsITvB";
+export const CTP_CLIENT_ID = "zRkq8wYQoN0eDPiwZ0-S2UU6";
 export const CTP_AUTH_URL = "https://auth.europe-west1.gcp.commercetools.com";
 export const CTP_API_URL = "https://api.europe-west1.gcp.commercetools.com";
 export const CTP_SCOPES = [
   "manage_my_orders:kratkoe",
+  "manage_customers:kratkoe",
   "create_anonymous_token:kratkoe",
   "view_categories:kratkoe",
   "manage_my_profile:kratkoe",
